a7c71116ae622d0301e9a6ac93c1049369b86c5f
AhmedAbdelkawy-hub <elkawy2@gmail.com>
2026-06-28 01:41:04 +0300

    Refactor BasicSeleniumTests to include setup and teardown methods, add URL and title assertions

--- src/test/java/demo/BasicSeleniumTests.java
+++ src/test/java/demo/BasicSeleniumTests.java
@@ -3,23 +3,42 @@ package demo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicSeleniumTests {
-    @Test
-    public void testExample() {
-        // Test implementation
-        // TestNG >> Test Suite >> Test >> Test Class >> Test Method
-        WebDriver driver;
+
+    // TestNG / JUnit > Test Workflow Management + Checkpoints / Assertions + ...etc
+    // TestNG >> Test Suite >> Test >> Test Class >> Test Method
+    // new browser session > browser action > element identification > element action > get element info / get browser info > checkpoint
+    // Assertion (Hard Assertion) X Verification (Soft Assertion)
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
         driver = new ChromeDriver(getOptimizedOptions());
-        driver.navigate().to("http://www.google.com");
+        driver.navigate().to("https://www.google.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkGooglePageUrl() {
         String currentUrl = driver.getCurrentUrl();
         System.out.println("Current URL: " + currentUrl);
+        Assert.assertEquals(currentUrl, "https://www.google.com/");
+    }
+
+    @Test
+    public void checkGooglePageTitle() {
         String currentPageTitle = driver.getTitle();
         System.out.println("Current Page Title: " + currentPageTitle);
-
-        // New browser session > Browser actions > element identification> element actions>get element info or get browser info> check point
-        driver.quit();
+        Assert.assertEquals(currentPageTitle, "Google");
     }
 
     public static ChromeOptions getOptimizedOptions() {
@@ -38,4 +57,5 @@ public class BasicSeleniumTests {
 
         return options;
     }
-}
+    }
+
